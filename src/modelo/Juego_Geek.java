ce740c6da487b40dbf0257d2911d9acbe8ab5386
federico <federico.barbetti@correounivalle.edu.co>
2023-05-08 10:33:48 -0500

    se genera la funcion entrega_Inactivos que maneja los dados inactivos

--- src/modelo/Juego_Geek.java
+++ src/modelo/Juego_Geek.java
@@ -19,8 +19,8 @@ definimos algunas variables constantes.
     definimos los arreglos.
      */
 
-    private Vector<Integer>tirada_Activos; // guarda el estado de los dados activos al inicio del juego
-    private Vector<Integer>tirada_Inactivos; // guarda el estado del os dados inactivos al inicio del juego.
+    private Vector<Integer> dados_Activos; // guarda el estado de los dados activos al inicio del juego
+    private Vector<Integer> dados_Inactivos; // guarda el estado del os dados inactivos al inicio del juego.
 
     private Vector<Integer>dados_Utilizados; // guarda los dados utilizados
 
@@ -44,6 +44,9 @@ definimos algunas variables constantes.
         tirada_DadosActivos();
         tirada_DadosInactivos();
         recibe_utilizados(99);//prueba
+        recibe_utilizados(88);//prueba
+        entrega_Inactivos(3);//prueba // reduce en 1 el arreglo dados_Inactivos.
+
 
         //recibe_utilizados(2);
 
@@ -53,7 +56,7 @@ definimos algunas variables constantes.
 
     public Vector<Integer> tirada_DadosActivos(){ // lanzamiento de los dados activos en el inicio de cada ronda.
         cara_Obtenida = new Dado();
-        tirada_Activos = new Vector<Integer>(cant_Dados_Actuvos_Iniciales);
+        dados_Activos = new Vector<Integer>(cant_Dados_Actuvos_Iniciales);
 
         String auxiliar = ""; // para probar en consola la funcionalidad
 
@@ -61,54 +64,59 @@ definimos algunas variables constantes.
 
         for(int i = 0; i < cant_Dados_Actuvos_Iniciales; i++){
             int receptor = cara_Obtenida.get_cara();
-           tirada_Activos.add(receptor);
+           dados_Activos.add(receptor);
            auxiliar = auxiliar + "La cara del dado [ "+ (i+1) + " ] es: " + cara_Obtenida.get_Nombre_Cara(receptor-1) + "\n";
 
         }
 
-        System.out.println(" \nTirada Inicial Activos-> " + tirada_Activos.toString());
+        System.out.println(" \nTirada Inicial Activos-> " + dados_Activos.toString());
         System.out.println(" \nTirada Inicial Activos Nombres->\n" + auxiliar.toString());
 
 
 
-        return tirada_Activos;
+        return dados_Activos;
 
     }
 
     public Vector<Integer> tirada_DadosInactivos(){// lanzamiento de los dados inactivos en el inicio de cada ronda.
         cara_Obtenida = new Dado();
-        tirada_Inactivos = new Vector<Integer>(cant_Dados_Inactivos_Iniciales);
+        dados_Inactivos = new Vector<Integer>(cant_Dados_Inactivos_Iniciales);
 
         String auxiliar = "";
 
         for(int i = 0; i < cant_Dados_Inactivos_Iniciales; i++){
             int receptor = cara_Obtenida.get_cara();
-            tirada_Inactivos.add(receptor);
+            dados_Inactivos.add(receptor);
             auxiliar = auxiliar + "La cara del dado [ "+ (i+1) + " ] es: " + cara_Obtenida.get_Nombre_Cara(receptor-1) + "\n";
 
 
         }
 
-        System.out.println(" \nTirada Inicial Inactivos -> " + tirada_Inactivos.toString());
+        System.out.println(" \nTirada Inicial Inactivos -> " + dados_Inactivos.toString());
         System.out.println(" \nTirada Inicial Inactivos Nombres->\n" + auxiliar.toString());
 
 
-        return tirada_Inactivos;
+        return dados_Inactivos;
 
     }
 
-    public Vector<Integer> recibe_utilizados(int paraRecibir){// Recibe los dados ya utilizados en un arreglo;
-        //cara_Obtenida = new Dado();
+    public Vector<Integer> recibe_utilizados(int paraRecibir){// actualiza (recibe) los dados ya utilizados en un arreglo;
+
         dados_Utilizados = new Vector<Integer>(10);// defaul size 10, cada campo es igual a 0;
         dados_Utilizados.setSize(10);
 
+
+        System.out.println(" al inicio del juego \n el tamaño del Vector tirada_Activos es " + dados_Activos.size() + "\n");
+
+
+       // dados_Utilizados.set(0,789); // para prueba
+
         String auxiliar = ""; // variable local
 
-        for(int i = 0; i < dados_Utilizados.size(); i++){
+        for(int i = 0; i < dados_Utilizados.size()-1; i++){
             if (dados_Utilizados.get(i)==null){
                 dados_Utilizados.set(i,paraRecibir);
                 auxiliar = auxiliar + "Dados Utilizados [ "+ i + " ] es: " + dados_Utilizados.get(i) + "\n";
-
                 break;
             }
 
@@ -119,6 +127,23 @@ definimos algunas variables constantes.
 
 
         return dados_Utilizados;
+
+    }
+/*
+Metodo que reduce en 1 el arreglo de dados_Inactivos, removiendo siempre la posición inicial.
+ */
+    public Vector<Integer> entrega_Inactivos(int paraEntregar){// actualiza (entrega) los dados inactivos en un arreglo;
+
+        paraEntregar = dados_Inactivos.remove(0);
+
+
+        System.out.println(" \n el tamaño del Vector dados_Inactivos es " + dados_Inactivos.size() + "\n");
+
+
+        System.out.println(dados_Inactivos.toString());
+
+
+        return dados_Inactivos;
 
     }
 
