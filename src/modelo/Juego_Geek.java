4c9c3e891d52ac01d9a30286374130911a6c0e27
federico <federico.barbetti@correounivalle.edu.co>
2023-05-08 21:30:15 -0500

    se crea la clase ProbadorEnConsola a fin de verificar en consola ciertas funcionalidades

--- src/modelo/Juego_Geek.java
+++ src/modelo/Juego_Geek.java
@@ -9,7 +9,7 @@ public class Juego_Geek {
 /*
 definimos algunas variables constantes.
  */
-    private int cant_Dados_Actuvos_Iniciales; // cantidad de dados  para iniciar el juego.
+    private int cant_Dados_Activos_Iniciales; // cantidad de dados  para iniciar el juego.
     private int cant_Dados_Inactivos_Iniciales; // candidad de dados inactivos al inicio del juego
 
     private int cuantas_Rondas; // define el numero de rondas a jugar.
@@ -32,7 +32,7 @@ definimos algunas variables constantes.
 
 
     public Juego_Geek() { // constructor
-        cant_Dados_Actuvos_Iniciales = 7;
+        cant_Dados_Activos_Iniciales = 7;
         cant_Dados_Inactivos_Iniciales=3;
         inicio();
 
@@ -41,11 +41,17 @@ definimos algunas variables constantes.
 
     public void inicio(){ // funcion inicializadora
         System.out.println("\nInicio el juego");
-        tirada_DadosActivos();
-        tirada_DadosInactivos();
-        recibe_utilizados(99);//prueba
-        recibe_utilizados(88);//prueba
-        entrega_Inactivos(3);//prueba // reduce en 1 el arreglo dados_Inactivos.
+//        tirada_DadosActivos();
+//
+//        tirada_DadosInactivos();
+//
+//        imprimeDatos();
+//
+//        recibe_utilizados(99);//prueba
+//        recibe_utilizados(88);//prueba
+//        entrega_Inactivos(3);//prueba // reduce en 1 el arreglo dados_Inactivos.
+
+        
 
 
         //recibe_utilizados(2);
@@ -56,15 +62,16 @@ definimos algunas variables constantes.
 
     public Vector<Integer> tirada_DadosActivos(){ // lanzamiento de los dados activos en el inicio de cada ronda.
         cara_Obtenida = new Dado();
-        dados_Activos = new Vector<Integer>(cant_Dados_Actuvos_Iniciales);
+        dados_Activos = new Vector<Integer>(cant_Dados_Activos_Iniciales);
 
         String auxiliar = ""; // para probar en consola la funcionalidad
 
 
 
-        for(int i = 0; i < cant_Dados_Actuvos_Iniciales; i++){
-            int receptor = cara_Obtenida.get_cara();
-           dados_Activos.add(receptor);
+        for(int i = 0; i < cant_Dados_Activos_Iniciales; i++){
+            int receptor = cara_Obtenida.get_cara(); // obtiene el valor random (int)
+           dados_Activos.add(receptor);// adiciona el valor al arreglo
+
            auxiliar = auxiliar + "La cara del dado [ "+ (i+1) + " ] es: " + cara_Obtenida.get_Nombre_Cara(receptor-1) + "\n";
 
         }
@@ -152,11 +159,14 @@ Metodo que reduce en 1 el arreglo de dados_Inactivos, removiendo siempre la posi
 
         cara_Obtenida = new Dado();
         dados_Inactivos = new Vector<Integer>(cant_Dados_Inactivos_Iniciales);
-        dados_Activos = new Vector<Integer>(cant_Dados_Actuvos_Iniciales);
+        dados_Activos = new Vector<Integer>(cant_Dados_Activos_Iniciales);
         dados_Utilizados = new Vector<Integer>(10);
 
         entrega_Inactivos(cara_Recibida); // resta 1 dado de los inactivos.
         recibe_utilizados(cara_Recibida); // adiciona 1 dado a los dados utilizados
+
+
+
 
         /*
         falta una funcion que busque el corazon (que se está jugando) y lo reemplaze por un nuevo lanzamiento.
@@ -169,6 +179,49 @@ Metodo que reduce en 1 el arreglo de dados_Inactivos, removiendo siempre la posi
         return dados_Activos;
 
     }
+    
+    
+    public void imprimeDatos() {
+
+
+        for (int i = 0; i < dados_Activos.size(); i++) {
+
+            if (dados_Activos.elementAt(i)== 1){
+
+                System.out.println(" es un corazon");
+            }
+
+        }
+
+    }
+
+    public Vector<Integer> borra_Activos_Jugados(int caraDado) {// caraDado tiene rango [1 -> 6]
+
+       // dados_Activos = new Vector<Integer>(cant_Dados_Activos_Iniciales);
+
+
+        for (int i = 0; i < dados_Activos.size(); i++) {
+
+            if (dados_Activos.elementAt(i)== caraDado){
+
+                dados_Activos.removeElementAt(i);
+
+                System.out.println(" prueba con corazon " + dados_Activos.size());
+
+                break;
+            }
+
+        }
+
+        System.out.println(" Vector sin un corazon " + dados_Activos.toString());
+
+
+        return dados_Activos;
+
+    }
+
+
+
 
 
 
